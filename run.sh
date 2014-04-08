9c50eec7308500501f71e5e569152f62a694e95f
Dr. Azrael Tod <code@g33ky.de>
2014-04-08 14:16:39 +0200

    stdbuf to remove buffer-lag to log

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONIOENCODING=utf-8
-nohup ./freenode.sh 0<&- &> grep -v "PING :" >> ./log/freenode.log &
-nohup ./euirc.sh 0<&- &> grep -v "PING :" >> ./log/euirc.log &
+stdbuf -oL -eL nohup ./freenode.sh 0<&- &> grep -v "PING :" >> ./log/freenode.log &
+stdbuf -oL -eL nohup ./euirc.sh 0<&- &> grep -v "PING :" >> ./log/euirc.log &
