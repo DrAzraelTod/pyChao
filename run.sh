4bf6ed54cc4d65892bf0289a3088ef92634dabb2
Dr. Azrael Tod <code@g33ky.de>
2014-04-11 20:14:20 +0200

    now with errors in log

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export PYTHONIOENCODING=utf-8
-stdbuf -oL -eL nohup ./freenode.sh 0<&- &> grep -v "PING :" >> ./log/freenode.log &
-stdbuf -oL -eL nohup ./euirc.sh 0<&- &> grep -v "PING :" >> ./log/euirc.log &
+#stdbuf -oL -eL nohup ./freenode.sh 0<&- &> grep -v "PING :" >> ./log/freenode.log &
+#stdbuf -oL -eL nohup ./euirc.sh 0<&- &> grep -v "PING :" >> ./log/euirc.log &
+stdbuf -oL -eL nohup ./freenode.sh 2>&1 | stdbuf -oL -eL grep -v "PING :" >> ./log/freenode.log &
+stdbuf -oL -eL nohup ./euirc.sh 2>&1 | stdbuf -oL -eL grep -v "PING :" >> ./log/euirc.log &
